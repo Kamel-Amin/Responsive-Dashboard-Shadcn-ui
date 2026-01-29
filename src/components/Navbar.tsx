2e3530ecf61f5e68e45c55131c72a59a7353a92b
Kamel-Amin <kamelamin298@gmail.com>
2026-01-29 02:13:37 +0200

    last preview commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,14 @@
-import { Moon } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { LogOut, Moon, Settings, User } from 'lucide-react';
 import Link from 'next/link';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 const Navbar = () => {
   return (
@@ -10,6 +19,30 @@ const Navbar = () => {
       <div className='flex items-center gap-4'>
         <Link href='/'>Dashboard</Link>
         <Moon />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar>
+              <AvatarImage src='https://github.com/shadcn.png' />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className='h-[1.2rem] w-[1.2rem] mr-2' />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className='h-[1.2rem] w-[1.2rem] mr-2' />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant='destructive'>
+              <LogOut className='h-[1.2rem] w-[1.2rem] mr-2' />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
