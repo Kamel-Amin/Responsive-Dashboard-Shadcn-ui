6b08d629608c232f949ad814287c13f31837374c
Kamel-Amin <kamelamin298@gmail.com>
2026-01-30 21:53:33 +0200

    Creating Area chart comp.

--- src/components/AppBarChart.tsx
+++ src/components/AppBarChart.tsx
@@ -1,17 +1,13 @@
 'use client';
-
-import { type ChartConfig } from '@/components/ui/chart';
-
-const chartConfig = {
-  desktop: {
-    label: 'Desktop',
-    color: '#2563eb',
-  },
-  mobile: {
-    label: 'Mobile',
-    color: '#60a5fa',
-  },
-} satisfies ChartConfig;
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from '@/components/ui/chart';
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 const chartData = [
   { month: 'January', desktop: 186, mobile: 80 },
@@ -22,8 +18,40 @@ const chartData = [
   { month: 'June', desktop: 214, mobile: 140 },
 ];
 
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: 'var(--chart-1)',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: 'var(--chart-4)',
+  },
+} satisfies ChartConfig;
+
 const AppBarChart = () => {
-  return <div>AppBarChart</div>;
+  return (
+    <div className=''>
+      <h1 className='text-lg font-medium mb-6'>Total Revenue</h1>
+      <ChartContainer config={chartConfig} className='min-h-50 w-full'>
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />{' '}
+          <XAxis
+            dataKey='month'
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey='desktop' fill='var(--color-desktop)' radius={4} />
+          <Bar dataKey='mobile' fill='var(--color-mobile)' radius={4} />
+        </BarChart>
+      </ChartContainer>
+    </div>
+  );
 };
 
 export default AppBarChart;
